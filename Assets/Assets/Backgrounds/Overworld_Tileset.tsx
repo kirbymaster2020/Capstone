d7b97876fbd1960b32c45f264c30f82e10d27a29
Ayo9295 <85228415+Ayo9295@users.noreply.github.com>
2025-03-02 22:02:20 -0500

    map

---
+++ Assets/Assets/Backgrounds/Overworld_Tileset.tsx
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Overworld_Tileset" tilewidth="10" tileheight="10" tilecount="560" columns="28">
+ <image source="Top_Down_Adventure_Pack_v.1.0/Overworld_Tileset.png" width="288" height="208"/>
+ <tile id="172">
+  <properties>
+   <property name="collision" value="true"/>
+  </properties>
+ </tile>
+ <tile id="173">
+  <properties>
+   <property name="collision" value="true"/>
+  </properties>
+ </tile>
+ <tile id="174">
+  <properties>
+   <property name="collision" value="true"/>
+  </properties>
+ </tile>
+ <tile id="200">
+  <properties>
+   <property name="collision" value="true"/>
+  </properties>
+ </tile>
+ <tile id="201">
+  <properties>
+   <property name="collision" value="true"/>
+  </properties>
+ </tile>
+ <tile id="202">
+  <properties>
+   <property name="collision" value="true"/>
+  </properties>
+ </tile>
+ <tile id="425">
+  <properties>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="426">
+  <properties>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="427">
+  <properties>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="453">
+  <properties>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="454">
+  <properties>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="455">
+  <properties>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="481">
+  <properties>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="482">
+  <properties>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="483">
+  <properties>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="509">
+  <properties>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="510">
+  <properties>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="511">
+  <properties>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+</tileset>
